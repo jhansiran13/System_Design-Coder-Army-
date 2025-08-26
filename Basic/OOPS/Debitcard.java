f45d141963c59e61864f7ab46b2ce7814be8ee27
jhansiran13 <cheggmudraboina@gmail.com>
2025-08-26 11:20:51 +0530

    2

--- Basic/OOPS/Debitcard.java
+++
@@ -1,9 +0,0 @@
-public class Debitcard extends Card {
-    public Debitcard(String userName,String cardNo){
-        super(userName,cardNo);
-    }
-    @Override
-    public void pay(){
-        System.out.println("Making payment via Debitcard");
-    }
-}
